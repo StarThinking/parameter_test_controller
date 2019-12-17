786c3675d177e6d42169d7092a7d0b1b456b4b93
root <ma.1189@osu.edu>
2019-12-17 01:03:10 -0500

    add a install sh

---
+++ install.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+sudo apt-get update
+sudo apt-get -y upgrade
+sudo apt-get -y install vim
+
+sudo apt-get -y install software-properties-common
+sudo apt-get -y install openjdk-8-jdk
+sudo apt-get update
+sudo apt-get -y install maven
+sudo apt-get -y install build-essential autoconf automake libtool cmake zlib1g-dev pkg-config libssl-dev
+#
+sudo apt-get -y install libprotobuf-java
+wget https://github.com/protocolbuffers/protobuf/releases/download/v2.5.0/protobuf-2.5.0.tar.gz
+tar zxvf protobuf-2.5.0.tar.gz
+cd protobuf-2.5.0/
+./configure
+make
+make check
+sudo make install
+cd ~
+
+# Optional packages
+sudo apt-get -y install snappy libsnappy-dev
+sudo apt-get -y install bzip2 libbz2-dev
+sudo apt-get -y install libjansson-dev
+sudo apt-get -y install fuse libfuse-dev
+sudo apt-get -y install zstd
+
+sudo ldconfig
+echo "export JAVA_HOME=/usr/lib/jvm/java-1.8.0-openjdk-amd64" >> ~/.profile
+echo "export HADOOP_HOME=/root/hadoop-3.1.2-src/hadoop-dist/target/hadoop-3.1.2" >> ~/.profile
+
+wget https://archive.apache.org/dist/hadoop/common/hadoop-3.1.2/hadoop-3.1.2-src.tar.gz
+tar zxvf hadoop-3.1.2-src.tar.gz
+
+mvn package -Pdist,native -DskipTests -Dtar
