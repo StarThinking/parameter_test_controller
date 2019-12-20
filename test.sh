6b8c6bcf7f5152d1ba45aed27387f063c8acfe42
root <ma.1189@osu.edu>
2019-12-19 23:29:50 -0600

    modify test.sh

--- test.sh
+++ test.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
+component=$1
+cd /root/parameter_test_controller
+javac Controller.java
 while IFS= read -r parameter
 do
-    java Controller $parameter > "$parameter".txt
-done < "./input.txt"
+    java Controller $parameter $component > /root/parameter_test_controller/"$parameter".txt
+done < "/root/parameter_test_controller/input.txt"
