02cdba114ea7a9cf34bb82979cf235547530bd03
root <root@node-2.hdfs-wis-10.postman-pg0.wisc.cloudlab.us>
2020-01-03 00:46:30 -0600

    distribution sh

--- test.sh
+++ test.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 
-component=$1
-cd /root/parameter_test_controller
-javac Controller.java
-while IFS= read -r parameter
+component=NameNode
+paras=( $(cat input.txt) )
+para_num=${#paras[@]}
+echo "para_num = $para_num"
+for i in $(seq 0 $(( para_num - 1 )))
 do
-    java Controller $parameter $component > /root/parameter_test_controller/"$parameter".txt
-done < "/root/parameter_test_controller/input.txt"
+    echo "${paras[$i]} on hadoop-$i"
+    ssh hadoop-$i "cd parameter_test_controller; java Controller ${paras[$i]} $component > /dev/null &" & 
+done
+
+sleep 10
+echo 'jobs distributed'
