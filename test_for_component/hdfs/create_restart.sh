045b6cc354663bbcfb12ed47222945b13ef74a6f
StarThinking <ma.1189@osu.edu>
2020-02-02 21:47:18 -0600

    int start/restart tests with issue in oneshot

--- test_for_component/hdfs/create_restart.sh
+++ test_for_component/hdfs/create_restart.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
+root_dir=~/parameter_test_controller/test_for_component/hdfs
+
 for c in namenode datanode journalnode
 do
     for i in $(cat start_$c.txt)
     do 
-	if [ "$(grep $i backup_all_tests/restart_$c.txt)" != "" ]; then 
+	if [ "$(grep $i $root_dir/component_test_in_method_filter_success/restart_$c.txt)" != "" ]; then 
 	    echo $i >> restart_$c.txt 
 	 fi
     done 
