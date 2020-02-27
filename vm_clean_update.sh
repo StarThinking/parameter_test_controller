74f8483b06a5fdbf6a6d22fb0ade4ac1946f63ca
ma.1189@osu.edu <ma.1189@osu.edu>
2020-02-26 23:30:33 -0600

    update

--- vm_clean_update.sh
+++ vm_clean_update.sh
@@ -3,5 +3,5 @@
 for i in $(seq 0 9)
 do 
     echo $i
-    ssh hadoop-$i "pkill -9 java; cd ~/parameter_test_controller; rm *.txt; git clean -df; git checkout -- *; git pull; javac Controller.java"
+    ssh hadoop-$i "pkill -9 java; cd ~/parameter_test_controller; rm *.txt; git clean -df; git checkout -- *; git pull; javac Controller.java; javac Hypothesis.java"
 done
