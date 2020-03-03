47b8b7ed0522e13d2920c2a3da3db7eae61690b2
root <root@node-0.c220g2-wis.postman-pg0.wisc.cloudlab.us>
2020-03-03 15:50:20 -0600

    allow repeats as input for Hypothesis test

--- Hypothesis.java
+++ Hypothesis.java
@@ -9,13 +9,13 @@ import java.text.SimpleDateFormat;
 
 public class Hypothesis extends Controller {
 
-    public static void hypothesisTestLogic(String parameter, String component, String test, String v1, String v2, String reconfPoint) {    
+    public static void hypothesisTestLogic(int repeats, String parameter, String component, String test, String v1, String v2, String reconfPoint) {    
         myPrint("hypothesisTestLogic");
         List<String> testSet = new ArrayList<String>();
         testSet.add(test);
-	int v1v2Repeats = 100; 
+	int v1v2Repeats = repeats; 
 	int v1v2FailedCount = 0; 
-	int v1v1v2v2Repeats = 100; 
+	int v1v1v2v2Repeats = repeats; 
 	int v1v1v2v2FailedCount = 0; 
         int i = 0;
         for (i=0; i<v1v2Repeats; i++) {
@@ -50,6 +50,7 @@ public class Hypothesis extends Controller {
     }
 
     public static void main(String[] args) {
+	int repeats = 0;
         String parameter = "";
         String component = "";
         String test = "";
@@ -60,24 +61,29 @@ public class Hypothesis extends Controller {
         startTime = endTime = timeElapsed = 0; 
 
         //if (!(args.length >= onetestArgIndex && args.length <= onetestArgIndex+1)) {
-        if (args.length != 6) {
+        if (args.length != 7) {
             myPrint("Error: args length is " + args.length);
-            myPrint("Hypothesis parameter component test v1 v2 reconfPoint");
+            myPrint("Hypothesis repeats parameter component test v1 v2 reconfPoint");
             System.exit(1);
         }
        
-	parameter = args[0];
-	component = args[1];
-        test=args[2];
-        v1=args[3];
-        v2=args[4];
-        reconfPoint=args[5];
+	repeats = Integer.valueOf(args[0]);
+	parameter = args[1];
+	component = args[2];
+        test=args[3];
+        v1=args[4];
+        v2=args[5];
+        reconfPoint=args[6];
 
 	if (!component.equals("NameNode") && !component.equals("DataNode") && !component.equals("JournalNode") && !component.equals("None")) {
 	    myPrint("Error: wrong component " + component);
 	    System.exit(1);
 	}
-  	
+  	if (repeats <= 0) {
+	    myPrint("Error: wrong repeats " + repeats);
+	    System.exit(1);
+	}
+
         String dummyParameterType = "Boolean";
         loadStaticTestData(dummyParameterType);
         
@@ -93,6 +99,7 @@ public class Hypothesis extends Controller {
             e.printStackTrace();
         }
 	
+	myPrint("repeats: " + repeats); 
 	myPrint("parameter: " + parameter); 
         myPrint("component: " + component); 
         myPrint("test: " + test);
@@ -101,7 +108,7 @@ public class Hypothesis extends Controller {
         myPrint("reconfPoint: " + reconfPoint);
 
 	startTime = System.nanoTime();
-        hypothesisTestLogic(parameter, component, test, v1, v2, reconfPoint);
+        hypothesisTestLogic(repeats, parameter, component, test, v1, v2, reconfPoint);
         endTime = System.nanoTime();
         timeElapsed = endTime - startTime;
         myPrint("Total execution time in seconds : " + timeElapsed / 1000000000);
