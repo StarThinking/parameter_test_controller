04811ad67c5023dd3698405a6bbdf5acc2cccb1f
root <root@node-1.reconf-v3.postman-pg0.wisc.cloudlab.us>
2020-03-19 00:45:27 -0500

    update sh

--- vm_clean_update.sh
+++ vm_clean_update.sh
@@ -4,5 +4,5 @@ for i in $(seq 0 9)
 do 
     echo $i
     ssh hadoop-$i "killall -9 bash"
-    ssh hadoop-$i "pkill -9 java; cd ~/parameter_test_controller; rm *.txt; git clean -df; git checkout -- *; git pull; javac Controller.java; javac Hypothesis.java"
+    ssh hadoop-$i "pkill -9 java; cd ~/parameter_test_controller; rm *.txt; git clean -df; git checkout -- *; git pull; javac *.java"
 done
