f008566789d588f245833131666112148280ee57
root <root@node-3.v3-cluster2.postman-pg0.wisc.cloudlab.us>
2020-03-24 12:11:06 -0500

    update

--- vm_clean_update.sh
+++ vm_clean_update.sh
@@ -3,6 +3,6 @@
 for i in $(seq 0 19)
 do 
     echo $i
-    docker exec -it hadoop-$i bash -c "killall -9 bash"
-    docker exec -it hadoop-$i bash -c "pkill -9 java; cd ~/parameter_test_controller; rm *.txt; git clean -df; git checkout -- *; git pull; javac *.java"
+    docker exec hadoop-$i bash -c "killall -9 bash"
+    docker exec hadoop-$i bash -c "pkill -9 java; cd /root/parameter_test_controller; rm *.txt; git clean -df; git checkout -- *; git pull; javac *.java"
 done
