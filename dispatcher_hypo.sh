d4154c45e950a74e9659740aeff13764b78e6f20
root <ma.1189@osu.edu>
2020-03-25 15:35:20 -0700

    add dispatcher_hypo.sh

---
+++ dispatcher_hypo.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+IFS=$'\n' 
+REPEAT=10
+entry_list=( $(cat task.txt) )
+entry_list_length=${#entry_list[@]}
+entry_cursor=0
+echo entry_list_length = $entry_list_length
+
+while [ $entry_cursor -lt $entry_list_length ]
+do
+    for i in $(seq 0 19)
+    do
+        jps_num=$(docker exec hadoop-$i bash -c "jps" | wc -l)
+        if [ $jps_num -gt 1 ]; then
+	     echo hadoop-$i is busy
+	else
+	    docker exec -d hadoop-$i bash -c "cd ~/parameter_test_controller; java Hypothesis $REPEAT ${entry_list[$entry_cursor]}"
+	    echo hadoop-$i is idle, assign entry $entry_cursor to it
+	    entry_cursor=$(( entry_cursor + 1 ))
+	    if [ $entry_cursor -ge $entry_list_length ]; then echo finish all tasks; break; fi
+	fi
+    done
+    sleep 10
+done
