e26ff117b440ae9121ca5de1bd8c29cb23465093
root <ma.1189@osu.edu>
2020-03-25 05:17:27 +0000

    update

--- create_containers.sh
+++ create_containers.sh
@@ -1,20 +1,21 @@
 #!/bin/bash
 
-num=19
-
-apt-get update
-apt-get upgrade
-
-sudo mkfs.ext4 /dev/sdc 
-mkdir /root/vm_images
-sudo mount /dev/sdc  /root/vm_images
-
-apt install -y docker.io docker-compose
-
-docker rm -f $(docker ps -aq); docker rmi -f $(docker images -q)
-systemctl stop docker
-rm -rf /var/lib/docker
-mkdir /var/lib/docker
-mount --rbind /root/vm_images /var/lib/docker
-systemctl start docker
-for i in $(seq 0 $num); do docker run -d -it --name hadoop-$i sixiangma/hdfs-reconf-template:v0.7_2020.03.24; done
+if [ $# -ne 2 ]; then echo wrong; exit -1; fi
+num=$1
+tag=$2
+#apt-get update
+#apt-get upgrade
+#
+#sudo mkfs.ext4 /dev/sdc 
+#mkdir /root/vm_images
+#sudo mount /dev/sdc  /root/vm_images
+#
+#apt install -y docker.io docker-compose
+#
+#docker rm -f $(docker ps -aq); docker rmi -f $(docker images -q)
+#systemctl stop docker
+#rm -rf /var/lib/docker
+#mkdir /var/lib/docker
+#mount --rbind /root/vm_images /var/lib/docker
+#systemctl start docker
+for i in $(seq 0 $num); do docker run -d -it --name hadoop-$i sixiangma/hdfs-reconf-template:$tag ; done
