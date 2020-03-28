e254f102e54029872da0024f8737017f7f763c10
root <root@node-0.v3-cluster2.postman-pg0.wisc.cloudlab.us>
2020-03-28 17:45:10 -0500

    updat

--- container_utility_sh/list_files_key.sh
+++ container_utility_sh/list_files_key.sh
@@ -7,7 +7,7 @@ key=$2
 
 for i in $(seq 0 $num)
 do
-    docker exec hadoop-$i bash -c "/root/parameter_test_controller/container_utility_sh/list_files_key_stub.sh $num $key" &
+    docker exec hadoop-$i bash -c "/root/parameter_test_controller/container_utility_sh/list_files_key_stub.sh $num '$key'" &
     pids[$i]=$!
 done
 
