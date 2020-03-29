4aafa38b0c2411cf0f431f31ed4b743e1b9d9b43
root <ma.1189@osu.edu>
2020-03-29 05:33:51 +0000

    add per_unit_test_run_with_v.sh

--- tests/hdfs/per_unit_test_run_with_v.sh
+++ tests/hdfs/per_unit_test_run_with_v.sh
@@ -1,20 +1,21 @@
 #!/bin/bash
 
-if [ $# -ne 5 ]; then echo 'wrong'; exit -1; fi
+if [ $# -ne 4 ]; then echo 'wrong: unit_test vv_mode parameter v1'; exit -1; fi
 
 unit_test=$1
 vv_mode=$2
 parameter=$3
 v1=$4
-v2=$5
 
-log_name="$unit_test"%"$vv_mode"%"$parameter"%"$v1"%"$v2"
+log_name="$unit_test"%"$vv_mode"%"$parameter"%"$v1"
 echo log_name=$log_name
 
 echo "$vv_mode" > ~/parameter_test_controller/shared/reconf_vvmode
 echo "$parameter" > ~/parameter_test_controller/shared/reconf_parameter
 echo "$v1" > ~/parameter_test_controller/shared/v1
-echo "$v2" > ~/parameter_test_controller/shared/v2
+echo "0:" > ~/parameter_test_controller/shared/v2 # dummy, never use as we just use v1v1 vv_mode
+echo "NameNode" > ~/parameter_test_controller/shared/reconf_component # dummy, ireelavent
+echo "1" > ~/parameter_test_controller/shared/reconf_point # dummy, ireelavent
 
 cd ~/hadoop-3.1.2-src/hadoop-hdfs-project
 mvn test -Dtest=$unit_test > /dev/null
