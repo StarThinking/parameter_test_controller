d18940c1e12ff3e6b777e5782a35bde0e7f87d6e
root <ma.1189@osu.edu>
2020-03-30 18:13:20 -0700

    add a simple java hp analysis program

---
+++ run_hp/hypo_output/ReconfTtest.java
@@ -0,0 +1,33 @@
+import org.apache.commons.math3.stat.inference.TTest;
+import java.util.Arrays;
+
+public class ReconfTtest {
+    public static void main(String[] args) {
+        if (args.length != 4) {
+            System.out.println("Error: ReconfTtest reconfSampleNum reconfSampleFailureNum nonReconfSampleNum nonReconfSampleFailureNum");
+            System.exit(-1);
+        }
+        final int reconfSampleNum = Integer.valueOf(args[0]);
+        final int reconfSampleFailureNum = Integer.valueOf(args[1]);
+        final int nonReconfSampleNum = Integer.valueOf(args[2]);
+        final int nonReconfSampleFailureNum = Integer.valueOf(args[3]);
+        final double succeedValue = 0;
+        final double failValue = 1;
+        double alpha = 0.01;
+
+        double[] reconfSamples = new double[reconfSampleNum];
+        Arrays.fill(reconfSamples, succeedValue);
+        for (int i=0; i<reconfSampleFailureNum; i++) {
+            reconfSamples[i] = failValue;
+        }
+        double[] nonReconfSamples = new double[nonReconfSampleNum];
+        Arrays.fill(nonReconfSamples, succeedValue);
+        for (int i=0; i<nonReconfSampleFailureNum; i++) {
+            nonReconfSamples[i] = failValue;
+        }
+        
+        TTest tt = new TTest();
+        boolean null_hypothesis = !tt.pairedTTest(reconfSamples, nonReconfSamples, alpha);
+        System.out.println("null_hypothesis is " + null_hypothesis);
+    }
+}
