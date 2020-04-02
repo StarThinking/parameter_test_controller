8d27636d1e1d6f4de7b9b641e6e616022d7b163c
root <root@node-8.6node-c220g5.postman-pg0.wisc.cloudlab.us>
2020-04-01 23:28:41 -0500

    modify clean_whole_cluster.sh

--- container_utility_sh/clean_whole_cluster.sh
+++ container_utility_sh/clean_whole_cluster.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
 
-if [ $# -ne 1 ]; then exit -1; fi
-num=$1
+if [ $# -ne 3 ]; then echo wrong pm vm tag; exit -1; fi
+pm=$1
+vm=$2
+tag=$3
 
-for i in $(seq 0 $num); do ssh node-$i "cd ~/parameter_test_controller ; git pull" & done; sleep 20;
-for i in $(seq 0 $num); do ssh node-$i "~/parameter_test_controller/container_utility_sh/rm_all_containers.sh 19"; done;
-for i in $(seq 0 $num); do ssh node-$i '~/parameter_test_controller/container_utility_sh/create_containers.sh 19 v0.9_2020.03.25'; done;
-for i in $(seq 0 $num); do ssh node-$i '~/parameter_test_controller/container_utility_sh/clean_update_all_container.sh 19' & pids[$i]=$!;  done; for p in ${pids[@]}; do wait $p; echo finished; done
+for i in $(seq 0 $pm); do ssh node-$i "cd ~/parameter_test_controller ; git pull" & done; sleep 20;
+
+for i in $(seq 0 $pm); do ssh node-$i "~/parameter_test_controller/container_utility_sh/rm_all_containers.sh $vm"; done;
+
+for i in $(seq 0 $pm); do ssh node-$i "~/parameter_test_controller/container_utility_sh/create_containers.sh $vm $tag"; done;
+
+for i in $(seq 0 $pm); do ssh node-$i "~/parameter_test_controller/container_utility_sh/clean_update_all_container.sh $vm" & pids[$i]=$!; done; 
+for p in ${pids[@]}; do wait $p; echo finished; done
