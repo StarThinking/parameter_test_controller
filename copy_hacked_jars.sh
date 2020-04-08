790209ed7c110d31ef4fddce5b32af1801f15edc
StarThinking <ma.1189@osu.edu>
2020-04-08 03:55:11 +0000

    move mylistener to common test and also modify pom.xml for hdfs, and romove reset in listener

--- copy_hacked_jars.sh
+++ copy_hacked_jars.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 
+# Hacked NameNode, DataNode, JournalNode
 cp ~/hadoop-3.1.2-src/hadoop-hdfs-project/hadoop-hdfs/target/hadoop-hdfs-3.1.2.jar  ~/.m2/repository/org/apache/hadoop/hadoop-hdfs/3.1.2/
+
+# Hacked Configured and newly added ReconfAgent
 cp ~/hadoop-3.1.2-src/hadoop-common-project/hadoop-common/target/hadoop-common-3.1.2.jar ~/.m2/repository/org/apache/hadoop/hadoop-common/3.1.2/
+
+# Newly added MyRunListener
+cp ~/hadoop-3.1.2-src/hadoop-common-project/hadoop-common/target/hadoop-common-3.1.2-tests.jar ~/.m2/repository/org/apache/hadoop/hadoop-common/3.1.2/
