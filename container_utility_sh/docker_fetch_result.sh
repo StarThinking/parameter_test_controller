af87c14a5575240ac44cbd32e3b9760a0528540e
root <root@node-0.10-c220g2.postman-pg0.wisc.cloudlab.us>
2020-05-02 01:59:38 -0500

    update

--- container_utility_sh/docker_fetch_result.sh
+++ container_utility_sh/docker_fetch_result.sh
@@ -3,12 +3,12 @@
 if [ $# -ne 1 ]; then echo wrong: num; exit -1; fi
 
 num=$1
-key=dfs
+key='.txt'
 
 function fetch {
     d=$1
-    files=$(docker exec hadoop-$d bash -c "ls ~/parameter_test_controller/ | grep $key")
-    for f in ${files[@]}; do docker cp hadoop-$d:/root/parameter_test_controller/$f .  ; done
+    files=$(docker exec hadoop-$d bash -c "ls /root/parameter_test_controller/target | grep $key")
+    for f in ${files[@]}; do docker cp hadoop-$d:/root/parameter_test_controller/target/$f .  ; done
 }
 
 for i in $(seq 0 $num)
