ab6bec534f7ea06eadaa45631e16fdb38638ce94
root <root@node-0.10-c220g2.postman-pg0.wisc.cloudlab.us>
2020-05-02 15:59:05 -0500

    update

--- container_utility_sh/docker_fetch_result.sh
+++ container_utility_sh/docker_fetch_result.sh
@@ -8,7 +8,7 @@ key='_hypothesis_'
 function fetch {
     d=$1
     files=$(docker exec hadoop-$d bash -c "ls /root/parameter_test_controller/target | grep $key")
-    for f in ${files[@]}; do docker cp hadoop-$d:/root/parameter_test_controller/target/$f .  ; done
+    for f in ${files[@]}; do docker cp hadoop-$d:/root/parameter_test_controller/target/$f /root/parameter_test_controller/target/ ; done
 }
 
 for i in $(seq 0 $num)
