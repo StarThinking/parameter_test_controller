df98cbe2b3a2b624167d3490b8d02e52c147c7ee
root <root@node-0.10-c220g2.postman-pg0.wisc.cloudlab.us>
2020-05-02 01:42:13 -0500

    update

--- dispatcher.sh
+++ dispatcher.sh
@@ -1,24 +1,43 @@
 #!/bin/bash
 
+vm_num=19 # 20
 IFS=$'\n' 
 entry_list=( $(cat task.txt) )
 entry_list_length=${#entry_list[@]}
 entry_cursor=0
 echo entry_list_length = $entry_list_length
+cmd='echo /root/parameter_test_controller/procedure.sh ${entry_list[$entry_cursor]}'
+
+function is_busy {
+    i=$1
+    jps_num=$(docker exec hadoop-$i bash -c "jps" | wc -l)
+    #sh_num=$(docker exec hadoop-$i bash -c "ps aux | grep run_mvn_test.sh" | wc -l)
+    if [ $jps_num -gt 1 ] ; then
+	echo "true"
+    else
+	echo "false"
+    fi
+}
 
 while [ $entry_cursor -lt $entry_list_length ]
 do
-    for i in $(seq 0 19)
+    for i in $(seq 0 $vm_num)
     do
-        jps_num=$(docker exec hadoop-$i bash -c "jps" | wc -l)
-        if [ $jps_num -gt 1 ]; then
+        if [ "$(is_busy $i)" == "true" ]; then
 	     echo hadoop-$i is busy
 	else
-	    docker exec -d hadoop-$i bash -c "cd ~/parameter_test_controller; java ReconfTester ${entry_list[$entry_cursor]}"
-	    echo hadoop-$i is idle, assign entry $entry_cursor to it
+	    echo hadoop-$i is not busy, assign entry $entry_cursor to it
+	    docker exec -d hadoop-$i bash -c "$(eval $cmd)"
 	    entry_cursor=$(( entry_cursor + 1 ))
 	    if [ $entry_cursor -ge $entry_list_length ]; then echo finish all tasks; break; fi
 	fi
     done
     sleep 1
 done
+
+for i in $(seq 0 $vm_num)
+do
+    while [ "$(is_busy $i)" == "true" ]; do sleep 10; done
+done
+
+echo all nodes are unbusy now
