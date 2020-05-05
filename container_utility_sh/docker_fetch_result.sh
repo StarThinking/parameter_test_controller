35eb2af41fcbe0b949ea5d5f1a4f445ce7f807e2
root <root@node-0.10-c220g2.postman-pg0.wisc.cloudlab.us>
2020-05-05 16:38:58 -0500

    greep -F

--- container_utility_sh/docker_fetch_result.sh
+++ container_utility_sh/docker_fetch_result.sh
@@ -7,7 +7,7 @@ key=$2
 
 function fetch {
     d=$1
-    files=$(docker exec hadoop-$d bash -c "ls /root/parameter_test_controller/target | grep $key")
+    files=$(docker exec hadoop-$d bash -c "ls /root/parameter_test_controller/target | grep -F $key")
     for f in ${files[@]}; do docker cp hadoop-$d:/root/parameter_test_controller/target/$f /root/parameter_test_controller/target/ ; done
 }
 
