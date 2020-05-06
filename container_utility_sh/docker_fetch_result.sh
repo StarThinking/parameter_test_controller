9247d574f88cfa925a92de662f0dd2f7602430ef
root <root@node-1.20c220g5-cluster.postman-pg0.wisc.cloudlab.us>
2020-05-05 23:15:47 -0500

    make the name shorter a bit

--- container_utility_sh/docker_fetch_result.sh
+++ container_utility_sh/docker_fetch_result.sh
@@ -5,11 +5,15 @@ if [ $# -ne 2 ]; then echo wrong: num key; exit -1; fi
 num=$1
 key=$2
 
-host=$(cat /proc/sys/kernel/hostname | awk -F '.' '{print $1}')
+host=$(cat /proc/sys/kernel/hostname | awk -F '.' '{print $1}' | sed 's/node-//g')
 function fetch {
     d=$1
     files=$(docker exec hadoop-$d bash -c "ls /root/parameter_test_controller/target | grep -F $key")
-    for f in ${files[@]}; do prefix=$(echo $f | awk -F '.txt' '{print $1}'); docker cp hadoop-$d:/root/parameter_test_controller/target/$f /root/parameter_test_controller/target/"$prefix"."$host"."$d".txt; done
+    for f in ${files[@]}
+    do 
+        prefix=$(echo $f | awk -F '_hypothesis_' '{print $1}'); 
+	docker cp hadoop-$d:/root/parameter_test_controller/target/$f /root/parameter_test_controller/target/"$prefix""_hypothesis_""$host"."$d".txt
+    done
 }
 
 for i in $(seq 0 $num)
