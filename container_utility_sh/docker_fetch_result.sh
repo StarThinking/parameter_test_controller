9868b7674783e620e0dcd16690dd6ac30dc8e84a
root <root@node-0.10-c220g2.postman-pg0.wisc.cloudlab.us>
2020-05-05 21:06:42 -0500

    add host and vm id as suffix

--- container_utility_sh/docker_fetch_result.sh
+++ container_utility_sh/docker_fetch_result.sh
@@ -5,10 +5,11 @@ if [ $# -ne 2 ]; then echo wrong: num key; exit -1; fi
 num=$1
 key=$2
 
+host=$(cat /proc/sys/kernel/hostname | awk -F '.' '{print $1}')
 function fetch {
     d=$1
     files=$(docker exec hadoop-$d bash -c "ls /root/parameter_test_controller/target | grep -F $key")
-    for f in ${files[@]}; do docker cp hadoop-$d:/root/parameter_test_controller/target/$f /root/parameter_test_controller/target/ ; done
+    for f in ${files[@]}; do prefix=$(echo $f | awk -F '.txt' '{print $1}'); docker cp hadoop-$d:/root/parameter_test_controller/target/$f /root/parameter_test_controller/target/"$prefix"."$host"."$d".txt; done
 }
 
 for i in $(seq 0 $num)
