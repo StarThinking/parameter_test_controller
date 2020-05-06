d90a2ce7813bc38b34b3555a41fd0b10bd7ec3b7
root <root@node-0.10-c220g2.postman-pg0.wisc.cloudlab.us>
2020-05-05 20:22:46 -0500

    update

--- saved.sh
+++ saved.sh
@@ -2,7 +2,7 @@
 for i in $(grep -oP "node-[0-9]{1,2}$" /etc/hosts | sed 's/node-//g' | sort -n); do ssh node-$i "rm ~/nohup.txt; nohup ~/parameter_test_controller/dispatcher.sh > nohup.txt &" & pids[$i]=$!; done; for p in ${pids[@]}; do wait $p; echo "$p is done"; done
 
 # start hypo dispatcher
-for i in $(grep -oP "node-[0-9]{1,2}$" /etc/hosts | sed 's/node-//g' | sort -n); do sleep; ssh node-$i "rm ~/nohup.txt; nohup ~/parameter_test_controller/hypo_dispatcher.sh > nohup.txt &" & pids[$i]=$!; done; for p in ${pids[@]}; do wait $p; echo "$p is done"; done
+for i in $(grep -oP "node-[0-9]{1,2}$" /etc/hosts | sed 's/node-//g' | sort -n); do sleep 2; ssh node-$i "rm ~/nohup.txt; nohup ~/parameter_test_controller/dispatcher_hypo.sh> nohup.txt &" & pids[$i]=$!; done; for p in ${pids[@]}; do wait $p; echo "$p is done"; done
 
 # check finish ratio
 for i in $(grep -oP "node-[0-9]{1,2}$" /etc/hosts | sed 's/node-//g' | sort -n); do finished=$(ssh node-$i "cat ~/nohup.txt | grep assign | wc -l"); all=$(ssh node-$i "cat nohup.txt | head -n 1" | awk -F ' = ' '{print $2}'); ratio=$(echo "scale=2; $finished / $all" | bc); echo "$finished out of $all are finished $ratio"; done
