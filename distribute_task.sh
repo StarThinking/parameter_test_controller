6afa98d0928e8b598c61102154d16faaa8cfc5d9
root <root@node-0.1-node.postman-pg0.wisc.cloudlab.us>
2020-05-14 16:41:30 -0500

    update

--- distribute_task.sh
+++ distribute_task.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
-if [ $# -ne 1 ]; then echo 'wrong [file]'; exit -1; fi
+if [ $# -ne 2 ]; then echo 'wrong [file] [task_dir]'; exit -1; fi
 
 num=$(grep -oP "node-[0-9]{1,2}$" /etc/hosts | sed 's/node-//g' | sort -n | wc -l)
 all_task_file=$1
+task_dir=$2
 
 rm x*
 split -d -n l/$num $all_task_file
@@ -11,6 +12,6 @@ for i in $(seq 0 9); do mv x0$i x$i; done
 
 for i in $(seq 0 $(( num - 1 )))
 do
-    scp x$i node-$i:~/parameter_test_controller/task.txt
+    scp x$i node-$i:"$task_dir"/task.txt
 done
 rm x*
