b568cdbe2d2322f7445f0b6cbceda847bf3ddfa4
root <root@node-0.fzhou-qv71583.osu-nfs-test-pg0.wisc.cloudlab.us>
2020-05-17 00:53:17 -0500

    compresss -output.txt at each container

---
+++ container_utility_sh/docker_fetch_compress_result.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+if [ $# -ne 3 ]; then echo 'wrong: [key] [src_dir] [dst_dir']; exit -1; fi
+
+num=19
+key=$1
+src_dir=$2
+dst_dir=$3
+
+function fetch {
+    d=$1
+    files=$(docker exec hadoop-$d bash -c "ls $src_dir | grep -F $key")
+    for f in ${files[@]}
+    do
+        docker exec hadoop-$d bash -c "cd $src_dir; /root/reconf_test_gen/compress.sh $f" 
+        docker exec hadoop-$d bash -c "cd $src_dir; rm $f" 
+    done
+    
+    compressed_files=$(docker exec hadoop-$d bash -c "ls $src_dir | grep -F $key")
+    for f in ${compressed_files[@]}
+    do
+	docker cp hadoop-$d:/"$src_dir"/$f "$dst_dir"
+    done
+}
+
+for i in $(seq 0 $num)
+do
+    fetch $i &
+    pids[$i]=$!
+done
+
+for i in $(seq 0 $num)
+do
+    wait ${pids[$i]}
+done
+
+echo "all files with key $key are fetched"
--- container_utility_sh/just_update_all_container.sh
+++ container_utility_sh/just_update_all_container.sh
@@ -6,7 +6,7 @@ num=$1
 for i in $(seq 0 $num)
 do 
     echo $i
-    docker exec hadoop-$i bash -c "cd /root/parameter_test_controller; git pull" &
+    docker exec hadoop-$i bash -c "cd /root/parameter_test_controller; git pull; cd /root/reconf_test_gen; git pull; git checkout -- compress.sh" &
     pids[$i]=$!
 done
 
