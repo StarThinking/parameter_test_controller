aac45011b7334552de22f31b6b202e5e6eecb084
root <root@node-0.msx-18.postman-pg0.wisc.cloudlab.us>
2020-05-22 11:57:38 -0500

    update

---
+++ filter_suscipious.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+mkdir suspicious
+mv $(for i in *.txt; do ~/parameter_test_controller/hypo_analysis.sh $i; done) suspicious/
+cd suspicious
+# fetch corresponding run logs
+for c in *.txt; do for i in $(grep -oP "node-[0-9]{1,2}$" /etc/hosts | sed 's/node-//g' | sort -n); do ssh node-$i "rm ~/parameter_test_controller/target/*.txt; ~/parameter_test_controller/container_utility_sh/docker_fetch_result.sh "$(echo "$c" | awk -F '_hypothesis_' '{print $1}')"_run_ /root/parameter_test_controller/target/ /root/parameter_test_controller/target/"; scp node-$i:~/parameter_test_controller/target/*.txt .; done; done
