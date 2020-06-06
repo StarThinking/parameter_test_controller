dc56776c6791c4f08cad6ba251f5d29fb4b68fcf
StarThinking <ma.1189@osu.edu>
2020-06-06 21:00:41 +0000

    update sh

--- container_utility_sh/update_white_list_all_containers.sh
+++ container_utility_sh/update_white_list_all_containers.sh
@@ -6,7 +6,7 @@ num=$1
 for i in $(seq 0 $num)
 do 
     echo $i
-    docker exec hadoop-$i bash -c "cd /root/parameter_test_controller; git pull; cd /root/reconf_test_gen; git pull; git checkout -- compress.sh" &
+    docker exec hadoop-$i bash -c "cd /root/parameter_test_controller; git pull;" &
     pids[$i]=$!
 done
 
@@ -14,4 +14,4 @@ for i in $(seq 0 $num)
 do
     wait ${pids[$i]}
 done
-echo 'all containers are updated'
+echo 'all containers are git updated'
