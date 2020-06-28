13c5b9a598cfbffb2d71706475d2dd7e04363951
root <root@node-0.controller.postman-pg0.wisc.cloudlab.us>
2020-06-28 17:03:58 -0500

    delete some stale files

--- results/extract_tuple.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-IFS=$'\n'
-
-while read -r line
-do
-    raw_tuple=$(echo $line | awk -F '_hypothesis_|_run_' '{print $1}')
-    tuple=$(echo $raw_tuple | sed 's/%/ /g')
-    echo $tuple
-done
--- tests/hdfs/README.txt
+++
@@ -1,3 +0,0 @@
-For NameNode, multi_instance plus restart ---> restart, because we find multiple instance mode is useless.
-
-refer to collect_all_tests for information about collecting per unit test data.
