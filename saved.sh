16461c2ed8c3b03d6eb7c53f5b526bf8fbd17e61
root <root@node-0.220g5-1.postman-pg0.wisc.cloudlab.us>
2020-07-26 17:21:27 -0500

    update

--- saved.sh
+++ saved.sh
@@ -22,6 +22,8 @@ for i in $(grep -oP "node-[0-9]{1,2}$" /etc/hosts | sed 's/node-//g' | sort -n);
 # check hypo that contains might
 for i in *_hypothesis_*; do if [ "$(grep might $i)" != "" ]; then echo $i; cat $i | tail -n 5; echo "";  fi ; done
 
+cf=0.95; clean_sheet=1; for i in *_hypothesis_*; do ~/parameter_test_controller/hypo_analysis.sh $i $cf $clean_sheet; done | while read line; do cat $line | tail -n 5; echo ''; done
+
 # check if hypo unfinished
 for i in *_hypothesis_*; do if [ "$(grep 'Total execution time' $i)" == "" ]; then echo $i; fi; done
 
