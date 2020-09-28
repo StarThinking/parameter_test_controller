6f28d5e7b8153dd8532f01f62356569ea15432b6
root <root@node-0.eurosys21.postman-pg0.wisc.cloudlab.us>
2020-09-27 23:06:52 -0500

    update

---
+++ container_utility_sh/docker_fetch_result_by_key.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+if [ $# -ne 3 ]; then echo 'wrong: [key] [src_dir] [dst_dir']; exit -1; fi
+
+num=$(( $(cat /proc/cpuinfo | grep 'processor' | wc -l) / 2 ))
+if [ $num -gt 20 ]; then num=20; fi
+num=$(( num -1 ))
+key=$1
+src_dir=$2
+dst_dir=$3
+
+function fetch {
+    d=$1
+    #files=$(docker exec hadoop-$d bash -c "ls $src_dir | grep -F $key")
+    #for f in ${files[@]}
+    #do
+	rm -rf ~/my_tmp 
+	docker cp hadoop-$d:/"$src_dir" ~/my_tmp
+	ls ~/my_tmp/ | grep "$key" | xargs mv -t "$dst_dir"
+    #done
+}
+
+for i in $(seq 0 $num)
+do
+    fetch $i
+    #pids[$i]=$!
+done
+
+#for i in $(seq 0 $num)
+#do
+#    wait ${pids[$i]}
+#done
+
+echo "all files with key $key are fetched"
